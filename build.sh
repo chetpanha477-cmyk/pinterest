b84cf8ab6cf324b2f296d1e7a554203b798268ef
Chet Panha <chetpanha477@gmail.com>
2026-07-05 19:34:44 +0700

    Add --clear to collectstatic to fix stale static manifest error

--- build.sh
+++ build.sh
@@ -3,7 +3,5 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput --clear
 python manage.py migrate
-python manage.py create_demo_users
-python manage.py seed_demo_pins
